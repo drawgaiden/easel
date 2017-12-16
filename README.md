5239ced67f7308f73e0e458add52a98e75f0120d
Corey Schram <coreyschram@gmail.com>
2017-12-15 21:22:14 -0600

    Moved.

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# easel
-Canvas drawing application library.
+This project has been merged with the main [DrawGaiden repo](https://github.com/drawgaiden/drawgaiden) and no longer maintained here.
--- example/src/main.ts
+++
@@ -1,8 +0,0 @@
-import Easel from '../../src';
-import '../../src/style.scss';
-
-const easel = new Easel(document.getElementById('easel') as HTMLElement, {
-    width: 1000,
-    height: 1000
-});
-console.log(easel);
